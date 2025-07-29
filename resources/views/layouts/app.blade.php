d3888af6e0afd89ea873e64a2fe93c23d59bc20e
Yosa-web <mys310503@gmail.com>
2025-07-29 11:50:19 +0700

    bntr ya pii

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="id">
 <head>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Beranda')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css') {{-- Aktifkan jika pakai Vite & Tailwind --}}

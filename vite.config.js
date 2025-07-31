b9b4e5341540e87b0c3d42456eb595b7e40d2067
Yosa-web <mys310503@gmail.com>
2025-08-01 02:35:13 +0700

    comit

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+    host: true, // ini membuat Vite bisa diakses dari jaringan luar
+    strictPort: true,
+    port: 5173,
+    allowedHosts: ['.ngrok-free.app'], // ⬅ tambahkan ini
+  },
 });
